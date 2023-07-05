0330e57a69481573fbae32eeaf45c34d04d192ff
Zeynep Sumer <70263646+zeyneps99@users.noreply.github.com>
2023-07-05 23:32:48 +0300

    board setup complete

--- Match-3-Study/Assets/Scripts/Game/Base/Board.cs
+++ Match-3-Study/Assets/Scripts/Game/Base/Board.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class Board : MonoBehaviour
 {
     private int _width, _height;
+    private float _cubeWidth;
     private Cube[,] _cubes;
-    private Grid[,] _grids;
-    [SerializeField] private GameObject _gridPrefab;
     [SerializeField] private GameObject _cubePrefab;
-    [SerializeField] private Transform _gridContainer;
+    [SerializeField] private RectTransform _grid;
     [SerializeField] private Transform _cubeContainer;
+    
 
     private ObjectPool<Cube> _cubePool = new ObjectPool<Cube>();
 
@@ -21,35 +21,28 @@ public class Board : MonoBehaviour
         _width = level.Width;
         _height = level.Height;
         _cubes = new Cube[_width, _height];
-        _grids = new Grid[_width, _height];
 
-         if (_cubeContainer.TryGetComponent<GridLayoutGroup>(out var l))
+        if (_cubeContainer.TryGetComponent<GridLayoutGroup>(out var l))
         {
             l.constraint = GridLayoutGroup.Constraint.FixedRowCount;
             l.constraintCount = _width;
 
+            if(_cubePrefab.TryGetComponent<RectTransform>(out var cubeRT))
+            {
+                _cubeWidth = cubeRT.rect.width;
+                l.cellSize = Vector2.one * _cubeWidth;
+            }
         }
 
-        GenerateGrids();
         GenerateCubes();
+        GenerateGrid();
     }
 
-    private void GenerateGrids()
+    private void GenerateGrid()
     {
-        SetGridLayout(_gridContainer);
-        for (int i = 0; i < _width; i++)
+        if(_grid != null)
         {
-            for (int j = 0; j < _height; j++)
-            {
-                GameObject go = Instantiate(_gridPrefab, _gridContainer);
-                if (go.TryGetComponent<Grid>(out Grid grid))
-                {
-                    grid.name = "Grid - " + i + " , " + j;
-                    grid.Position = new Vector2Int(i, j);
-                    _grids[i, j] = grid;
-                }
-
-            }
+            _grid.sizeDelta = new Vector2(_height + .25f, _width + .25f) * _cubeWidth;
         }
     }
     //can also extract specified order of cubes from json file for further implementations
@@ -60,21 +53,25 @@ public class Board : MonoBehaviour
             return;
         }
         SetGridLayout(_cubeContainer);
-        foreach (Grid grid in _grids)
+        for (int i = 0; i < _width; i++)
         {
-            _cubePool.CreateInstance(_cubePrefab);
-            Cube newCube = _cubePool.Get();
+            for (int j = 0; j < _height; j++)
+            {
+                _cubePool.CreateInstance(_cubePrefab);
+                Cube newCube = _cubePool.Get();
+                newCube.name = "Cube - " + i + " , " + j;
+                newCube.transform.SetParent(_cubeContainer);
+                newCube.transform.localScale = Vector2.one;
+                var type = (CubeTypes)UnityEngine.Random.Range(1, 6);
+                newCube.SetType(type);
+                _cubes[i, j] = newCube;
 
-            newCube.name = "Cube - " + grid.Position.x + " , " + grid.Position.y;
-            newCube.transform.SetParent(_cubeContainer);
-            newCube.transform.position = grid.transform.position;
-            var type = (CubeTypes)UnityEngine.Random.Range(1, 6);
-            newCube.SetType(type);
-            _cubes[grid.Position.x, grid.Position.y] = newCube;
+            }
+            
         }
     }
 
-    
+
 
     private void SetGridLayout(Transform container)
     {
--- Match-3-Study/Assets/Scripts/Game/Base/Grid.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Grid : Entity
-{
-    public Vector2Int Position;
-
-}
